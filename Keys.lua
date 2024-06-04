53ca5a628dc4815e15a8bf271484cbb8241a8e62
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-04 18:56:27 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,5 +1,3 @@
 return {
-"5bf73999-3984-4f5e-a461-da842f446d6a", --DiscordUserId = "847907964257697803", --Activated = True
+"5bf73999-3984-4f5e-a461-da842f446d6a", --DiscordUserId = "847907964257697803", --Activated = False
 }
-
-
