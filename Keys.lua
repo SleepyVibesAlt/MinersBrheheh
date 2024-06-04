9cbb258388675704c1dd428eae10ad9907d53d29
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-04 13:59:41 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,6 +1,3 @@
 return {
-  "free", --DiscordUserId = "123",--Activated = False
-}
-
-"7ecc76cf-8c4a-47ea-a3ac-21331b19f531", --DiscordUserId = "847907964257697803", --Activated = False
 "5bf73999-3984-4f5e-a461-da842f446d6a", --DiscordUserId = "847907964257697803", --Activated = False
+}
