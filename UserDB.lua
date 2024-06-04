d5470422e0744aec0ef3a4e32b2abbb79b19aa37
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-04 19:27:51 -0400

    Update UserDB.lua

--- UserDB.lua
+++ UserDB.lua
@@ -1,1 +1,7 @@
-print(key)
+-- Your Lua script
+
+local KeyModule = require(game:GetService("ServerScriptService").KeyModule) -- or ReplicatedStorage
+
+local key = KeyModule.key
+
+print(key) -- This will print your key
