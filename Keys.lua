8a03baca2c40b8b4c75e2570c4e3b4ad4b136e3a
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-14 19:39:34 -0400

    Add new key

--- Keys.lua
+++ Keys.lua
@@ -6,3 +6,4 @@ return {
 
 "HwidNotSet" --Key = "32942815-6592-4e66-b7f9-a98e90077f02", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = False
 "HwidNotSet" --Key = "c661eb4d-b6dd-44a0-b3f3-31117946deb2", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = False
+"HwidNotSet" --Key = "10c615df-5873-4ce6-843b-1786b2786723", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = False
