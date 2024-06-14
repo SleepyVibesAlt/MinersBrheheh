495dae19ebdad347c541b14417f35765e537df69
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-14 13:56:15 -0400

    Add new key

--- Keys.lua
+++ Keys.lua
@@ -4,3 +4,4 @@ return {
 
 "5b04b58a-c78a-452d-94fb-80abb5d6e0cb", --DiscordUserId = "847907964257697803", --Activated = False
 "hwidnotsetyet" --Key = "01ef0e89-61e8-4514-ac18-523ce4b27bdd", --DiscordUserId = "847907964257697803", --Activated = False
+"hwidnotsetyet" --Key = "9f242bdd-3379-4280-83bf-718559ee7019", --DiscordUserId = "847907964257697803", --Activated = False
