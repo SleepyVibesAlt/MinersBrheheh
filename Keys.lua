5dd349defe7028ab14e663914adbe4ee2aa73685
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-14 18:53:15 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,10 +1,3 @@
 return {
 
 }
-
-"5b04b58a-c78a-452d-94fb-80abb5d6e0cb", --DiscordUserId = "847907964257697803", --Activated = True
-"hwidnotsetyet" --Key = "01ef0e89-61e8-4514-ac18-523ce4b27bdd", --DiscordUserId = "847907964257697803", --Activated = False
-"hwidnotsetyet" --Key = "9f242bdd-3379-4280-83bf-718559ee7019", --DiscordUserId = "847907964257697803", --Activated = False
-
-"HwidNotSet" --Key = "a73e3a39-a58c-4614-811b-673ce3e09032", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = True
-"HwidNotSet" --Key = "dee76673-0d7b-42fa-9ed7-6b0bd6c53e42", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = False
