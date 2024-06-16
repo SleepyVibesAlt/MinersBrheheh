5b501c9085da7d0b43b06c7f88557d8c8a2e5f5e
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 11:03:55 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,15 +1,3 @@
 return {
 
 }
-
-"HwidNotSet" --Key = "67408a79-59b3-4401-a770-75d3b3718eff", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = False
-
-
-"HwidNotSet" --Key = "59eab055-426d-4200-a59b-318bf1f6d884", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = False
-"HwidNotSet" --Key = "b880d360-e9a2-4bd1-bd8b-9f945c8d250b", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = True
-
-"HwidNotSet" --Key = "d4bd0d63-af03-48d8-9877-2ee97cbc453d", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = False
-"HwidNotSet" --Key = "ea94078f-890f-4638-ace3-a1cd979883d9", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = True
-
-"HwidNotSet" --Key = "d054a4f9-90e3-4d5b-8638-2a528aa94d96", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = False
-"HwidNotSet" --Key = "b89147fc-fa9d-4b66-bf26-faf6f2079814", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Activated = False
