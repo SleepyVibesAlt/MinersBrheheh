75f62219fd455b5f43c31d6f01a652dc31772b32
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 15:48:55 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -9,10 +9,13 @@ return {
             Activated = false
         },
         {
+            Hwid = "NotSet",
             Key = "7540ea77-199c-4301-ab33-aa3a12ee9f34",
-            Hwid = "HWID SETTED",
+            DiscordUserId = "123456789012345678",
+            RBLXUserId = "UserNotSet",
+            Blacklisted = false,
             Activated = false
-            RBLXUserId = "UpdatedRBLXUserId",
+        }
         {
             Hwid = "AnotherHwid",
             Key = "another-key-value",
