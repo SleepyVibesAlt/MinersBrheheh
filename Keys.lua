8282aaa4f3c75e7327ed00c6fe673dda62b3fea2
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 11:14:37 -0400

    Update key activation status

--- Keys.lua
+++ Keys.lua
@@ -1,3 +1,4 @@
 return {
-"HwidNotSet" --Key = "b99888fe-8740-47d1-9a5c-7422dd80b761", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Blacklisted = False, --Activated = False
+"HwidNotSet" --Key = "b99888fe-8740-47d1-9a5c-7422dd80b761", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Blacklisted = True, --Activated = True
 }
+
