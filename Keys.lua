3784e7dae880d4e0424789c4cfe03d4e92404a0d
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 16:14:31 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -26,30 +26,5 @@ return {
             Blacklisted = false,
             Activated = false
         },
-
-        {
-            Key = "1e993728-5ed0-4617-86c0-20ce062f55e7",
-            DiscordUserId = "847907964257697803",
-            RBLXUserId = "NotSet",
-            Hwid = "NotSet",
-            Blacklisted = false,
-            Activated = false
-        },
-        {
-            Hwid = "NotSet",
-            Key = "7540ea77-199c-4301-ab33-aa3a12ee9f34",
-            DiscordUserId = "123456789012345678",
-            RBLXUserId = "NotSet",
-            Blacklisted = false,
-            Activated = true
-        },
-        {
-            Hwid = "AnotherHwid",
-            Key = "another-key-value",
-            DiscordUserId = "123456789012345678",
-            RBLXUserId = "AnotherUserId",
-            Blacklisted = false,
-            Activated = false
-        }
     }
 }
