ee5de8033d20f113fdcded1322f619b633bd5ba5
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 16:13:49 -0400

    Add new key

--- Keys.lua
+++ Keys.lua
@@ -1,6 +1,15 @@
 return {
     ["Whitelist Data"] = {
         {
+            Key = "f6eadb8a-ed01-404d-b428-8f56db9a9258",
+            RBLXUserId = "UserNotSet",
+            Hwid = "NotSet",
+            DiscordUserId = "847907964257697803",
+            Blacklisted = false,
+            Activated = false
+        },
+
+        {
             Key = "a3bb5d60-78f9-4543-8be2-97a9accd8655",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
