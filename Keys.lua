92bc47bdd5aa33fd5dfd75bf4cfb0e6dc2de722a
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 11:14:00 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,6 +1,2 @@
 return {
-"HwidNotSet" --Key = "7540ea77-199c-4301-ab33-aa3a12ee9f34", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Blacklisted = False, --Activated = False
-"HwidNotSet" --Key = "24ca2e84-2d2c-4a97-8deb-69e7a7cd4e1c", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Blacklisted = False, --Activated = False
-"HwidNotSet" --Key = "1a69e285-0e23-4d80-8724-e5c78215bdbc", --DiscordUserId = "847907964257697803", --RBLXUserId = "UserIdNotSet", --Blacklisted = False, --Activated = False
-
 }
