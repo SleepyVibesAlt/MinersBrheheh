f258afc12fba6cce51407c302b8cfcf2ddce66a1
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 21:37:59 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -8,15 +8,5 @@ return {
             Blacklisted = false,
             Activated = false
         },
-
-        {
-            Key = "dfb36365-a727-46d6-9901-fe4d92b8996d",
-            RBLXUserId = "2323",
-            Hwid = "Not23232Set",
-            DiscordUserId = 847907964257697803,
-            DiscordUserId = 847907964257697803,
-            Blacklisted = false,
-            Activated = false
-        },
     }
 }
