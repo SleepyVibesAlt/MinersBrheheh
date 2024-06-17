82dd78a7b987b68a182ecc521acf379698e6a2ca
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 22:22:19 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -4,7 +4,7 @@ return {
             Key = "aff8500a-d18d-4c9e-be1f-4eefa54f7d95",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
+            DiscordUserId = 8479074964257697803,
             Blacklisted = false,
             Activated = true
         },
@@ -12,7 +12,7 @@ return {
             Key = "a7d7fa73-ef1b-4db0-84dd-f62bc8079133",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 8479079654257697803,
+            DiscordUserId = 84790796542576497803,
             Blacklisted = false,
             Activated = true
         },
@@ -20,7 +20,7 @@ return {
             Key = "a142df43-e947-4021-82f3-b4973ef1e197",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 8479507964257697803,
+            DiscordUserId = 84795079464257697803,
             Blacklisted = false,
             Activated = true
         },
