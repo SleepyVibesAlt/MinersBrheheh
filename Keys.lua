a3e9fc1bd98fe074980ee61ef968bb071e0347ee
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 23:27:31 -0400

    Remove Blacklisted = false below DiscordUserId = 847907964257697803

--- Keys.lua
+++ Keys.lua
@@ -1,13 +1,6 @@
 return {
-    ["Whitelist Data"] = {
-        {
-            Key = "9eaa3c22-2669-46f9-a76d-f9adb1de87e3",
-            RBLXUserId = "UserNotSet",
-            Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
-            Blacklisted = false,
-            Activated = true
-        },
-
-    }
-}
+["Whitelist Data"] = {
+{
+Key = "9eaa3c22-2669-46f9-a76d-f9adb1de87e3",
+RBLXUserId = "UserNotSet",
+Hwid = "NotSet",
