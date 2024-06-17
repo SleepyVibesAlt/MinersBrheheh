a33ccaf586485e8ba9ea11acf1ca76d89e64463c
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 23:17:24 -0400

    Remove entry below DiscordUserId = 847907964257697803

--- Keys.lua
+++ Keys.lua
@@ -4,8 +4,6 @@ return {
             Key = "28e4df7c-5013-41a0-8864-1c5dd7da135f",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
-            Blacklisted = false,
             Activated = true
         },
 
