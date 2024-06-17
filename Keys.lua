c9db6304570a0f90c82bace2626ddf5df9998dc2
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 23:17:16 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,12 +1,4 @@
 return {
     ["Whitelist Data"] = {
-        {
-            Key = "e28ca7a4-ab8f-4801-9803-4fa2e0cc4fb1",
-            RBLXUserId = "UserNotSet",
-            Hwid = "NotSet",
-            Blacklisted = false,
-            Activated = true
-        },
-
     }
 }
