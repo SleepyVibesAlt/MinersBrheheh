dd2c8b3b5d1e3e255861e017bc89612a9b0b0b5b
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 22:54:57 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,27 +1,4 @@
 return {
-{
-Key = "aff8500a-d18d-4c9e-be1f-4eefa54f7d95",
-RBLXUserId = "UserNotSet",
-Hwid = "NotSet",
-DiscordUserId = 847907964257697803,
-Blacklisted = false,
-Activated = true
-},
-{
-Key = "a7d7fa73-ef1b-4db0-84dd-f62bc8079133",
-RBLXUserId = "UserNotSet",
-Hwid = "NotSet",
-DiscordUserId = 84790796542576497803,
-Blacklisted = false,
-Activated = true
-},
-{
-Key = "a142df43-e947-4021-82f3-b4973ef1e197",
-RBLXUserId = "UserNotSet",
-Hwid = "NotSet",
-DiscordUserId = 84795079464257697803,
-Blacklisted = false,
-Activated = true
-},
-}
+    ["Whitelist Data"] = {
+    }
 }
