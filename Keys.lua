7863d29a74fa5b1a78f4cc69bd2bf42fc1f64986
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 23:11:19 -0400

    Remove entry below DiscordUserId = 847907964257697803

--- Keys.lua
+++ Keys.lua
@@ -4,7 +4,6 @@ return {
             Key = "75e8e86e-b774-440b-9e7d-6882a67ce233",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
             Blacklisted = false,
             Activated = true
         },
@@ -13,7 +12,6 @@ return {
             Key = "c01c2d92-d7da-4f84-9374-c71deb462d60",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
             Blacklisted = false,
             Activated = true
         },
@@ -22,7 +20,6 @@ return {
             Key = "f06a6c9d-8823-4037-81f9-056e5446cb6c",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
             Blacklisted = false,
             Activated = true
         },
