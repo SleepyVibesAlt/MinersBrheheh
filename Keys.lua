e934e81c3dc70fadd8c358f53fbe75421923359f
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-16 22:17:45 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -8,51 +8,21 @@ return {
             Blacklisted = false,
             Activated = true
         },
-
         {
             Key = "a7d7fa73-ef1b-4db0-84dd-f62bc8079133",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
+            DiscordUserId = 8479079654257697803,
             Blacklisted = false,
             Activated = true
         },
-
         {
             Key = "a142df43-e947-4021-82f3-b4973ef1e197",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
+            DiscordUserId = 8479507964257697803,
             Blacklisted = false,
             Activated = true
         },
-
-        {
-            Key = "9a8b7362-5ac4-41bb-b898-ecd5c439ae89",
-            RBLXUserId = "4124339158",
-            Hwid = "54D927F2-2E9D-4F9D-AB39-E8A02496D8B7",
-            DiscordUserId = 847907964257697803,
-            Blacklisted = false,
-            Activated = true
-        },
-
-        {
-            Key = "85c0fe77-4ce4-498e-a2d7-8c28d969bf35",
-            RBLXUserId = "UserNotSet",
-            Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
-            Blacklisted = false,
-            Activated = false
-        },
-
-        {
-            Key = "f60d1571-1730-41fa-b153-d19972ba65ee",
-            RBLXUserId = "tester",
-            Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
-            Blacklisted = false,
-            Activated = true
-        },
-
     }
 }
