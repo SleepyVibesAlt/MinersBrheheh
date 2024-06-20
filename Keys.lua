4563891ce50a6de68f531ae7e5b796e11f20f66d
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-20 14:08:59 -0400

    Add new key

--- Keys.lua
+++ Keys.lua
@@ -1,12 +1,16 @@
-return {
-
-}
+return {}
         {
             Key = "8f8c1697-149a-425b-9350-9bd0fee65323",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
             DiscordUserId = 847907964257697803,
             Blacklisted = false,
+            Activated = true, 
+            Key = "28199d4d-1b3e-4102-878f-d2eda0fa45ee",
+            RBLXUserId = "UserNotSet",
+            Hwid = "NotSet",
+            DiscordUserId = 847907964257697803,
+            Blacklisted = false,
             Activated = true
-        },
+    ,},
     
