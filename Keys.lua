8bc1ac60dc9a451ab3c4cc8c8ed973c31ac0a6b8
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-20 13:59:11 -0400

    Add new key

--- Keys.lua
+++ Keys.lua
@@ -1,5 +1,14 @@
 return {
-    {
+    
+{
+    Key = "13eca77e-838d-4602-960a-e9fa31beb2b6",
+    RBLXUserId = "UserNotSet",
+    Hwid = "NotSet",
+    DiscordUserId = 847907964257697803,
+    Blacklisted = false,
+    Activated = true
+},
+{
         Key = "9eaa3c22-2669-46f9-a76d-f9adb1de87e3",
         RBLXUserId = "UserNotSet",
         Hwid = "NotSet",
