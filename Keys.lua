02dab63fed25e483f300206f7b845418a95a25d2
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-20 14:11:01 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,16 +1,10 @@
-return {}
-        {
-            Key = "8f8c1697-149a-425b-9350-9bd0fee65323",
-            RBLXUserId = "UserNotSet",
-            Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
-            Blacklisted = false,
-            Activated = true, 
-            Key = "28199d4d-1b3e-4102-878f-d2eda0fa45ee",
-            RBLXUserId = "UserNotSet",
-            Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
-            Blacklisted = false,
-            Activated = true
-    ,},
-    
+return {
+    {
+        Key = "9eaa3c22-2669-46f9-a76d-f9adb1de87e3",
+        RBLXUserId = "UserNotSet",
+        Hwid = "NotSet",
+        DiscordUserId = 847907964257697803,
+        Blacklisted = false,
+        Activated = true
+    }
+}
