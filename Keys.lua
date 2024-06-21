e6046437d7b854f0290577d7c4bd7b09e42bea2f
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-21 06:18:54 -0400

    Set Blacklisted = false for DiscordUserId = 847907964257697803

--- Keys.lua
+++ Keys.lua
@@ -5,7 +5,7 @@ return {
             RBLXUserId = "4124339158",
             Hwid = "54D927F2-2E9D-4F9D-AB39-E8A02496D8B7",
             DiscordUserId = 847907964257697803,
-            Blacklisted = true
+            Blacklisted = false
             Activated = true
         },
     }
