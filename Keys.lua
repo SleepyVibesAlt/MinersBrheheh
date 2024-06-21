0bf25cee632d82728619c04478a8181c02c17761
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-21 16:09:22 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -4,7 +4,7 @@ return {
             Key = "d43069dc-dd6b-4c7f-8ce0-eea7856e610f",
             RBLXUserId = "UserNotSet",
             Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
+            DiscordUserId = 1234,
             Blacklisted = false,
             Activated = true
         },
