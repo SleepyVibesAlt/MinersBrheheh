4ab0af0df88f0e6602b6d557a0606b2cfe864ac8
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-21 18:57:05 -0400

    Update UserDB.lua

--- UserDB.lua
+++ UserDB.lua
@@ -1,12 +1,1 @@
-return {
-    ["Whitelist Data"] = {
-        {
-            Key = "583dfde9-6219-427b-a231-91c5e24344c9",
-            RBLXUserId = "UserNotSet",
-            Hwid = "NotSet",
-            DiscordUserId = 847907964257697803,
-            Blacklisted = false,
-            Activated = true
-        },
-    }
-}
+
