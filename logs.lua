c4c31e3096bd27ff1c087b76397079524c609641
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-22 19:50:10 -0400

    Update logs.lua

--- logs.lua
+++ logs.lua
@@ -1,9 +1,8 @@
 return {
     ["Whitelist Data"] = {
         {
-            Key = "583dfde9-6219-427b-a231-91c5e24344c9",
-            Reason = "test",
-            Online = false
+            Online = false,
+            Reason = "test"
         },
     }
 }
