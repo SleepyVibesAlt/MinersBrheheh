91b345e9b11640bcf6e0396dcfa2326312a992b8
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-22 18:30:54 -0400

    Update logs.lua

--- logs.lua
+++ logs.lua
@@ -1,8 +1,8 @@
 return {
     ["Whitelist Data"] = {
         {
-            Reason = "test"
-            Online = false,
+            Reason = "test",
+            Online = false
         },
     }
 }
