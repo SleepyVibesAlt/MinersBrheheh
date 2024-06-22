ca7cc1271112818198171e2e7508c15c4e5d1603
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-22 18:40:38 -0400

    Delete logs2.lua

--- logs2.lua
+++
@@ -1,9 +0,0 @@
-return {
-    ["Whitelist Data"] = {
-        {
-            Key = "583dfde9-6219-427b-a231-91c5e24344c9",
-            Reason = "test",
-            Online = false
-        },
-    }
-}
