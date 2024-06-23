33e1b1d4ff17f3e56666bdc26665229d5fc081be
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-23 01:08:31 -0400

    Reset HWID

--- Keys.lua
+++ Keys.lua
@@ -2,8 +2,8 @@ return {
     ["Whitelist Data"] = {
         {
             Key = "583dfde9r-6219-427b-a231-91c5e24344c9",
-            RBLXUserId = "6193395998",
-            Hwid = "54D927Fr2-2E9D-4F9D-AB39-E8A02496D8B7",
+            RBLXUserId = "UserNotSet",
+            Hwid = "NotSet",
             DiscordUserId = 847907964257697803,
             Blacklisted = false,
             Activated = true
