7f3e760811f567e61a95300504292c0076f008cb
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-26 19:36:23 -0400

    Set Blacklisted = true for DiscordUserId = 847907964257697803

--- Keys.lua
+++ Keys.lua
@@ -5,7 +5,7 @@ return {
             RBLXUserId = "6201199870",
             Hwid = "54D927F2dsfghfdsa-2E9D-4F9D-AB39-E8A02496D8B7",
             DiscordUserId = 847907964257697803,
-            Blacklisted = false,
+            Blacklisted = true,
             Activated = true
         },
     }
