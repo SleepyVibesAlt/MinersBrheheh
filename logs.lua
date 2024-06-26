7c3413e043ca2833cda8ce2cc02c25a1359104f9
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-06-26 19:02:21 -0400

    Disabled with reason: 21

--- logs.lua
+++ logs.lua
@@ -1,8 +1,8 @@
 return {
     ["Whitelist Data"] = {
         {
-            Online = true,
-            Reason = "RAGHHH"
+            Online = false,
+            Reason = "21"
         },
     }
 }
