35d6e83c940701fe8036e1ef02fd17ded4a2f30b
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-02 17:53:00 -0400

    Update file via bot

--- Keys.lua
+++ Keys.lua
@@ -11,8 +11,8 @@ return {
 
         {
             Key = "28296bbb-436b-45e1-82eb-85e9f9b746d9",
-            RBLXUserId = "UserNotSet",
-            Hwid = "NotSet",
+            RBLXUserId = "7004832285",
+            Hwid = "54D927F2-2E9D-4F9D-AB39-E8A02496D8B7",
             DiscordUserId = 847907964257697803,
             Blacklisted = false,
             Activated = true
