7378dc9fb2a9592e44d356cd5fe4626914eb41b4
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-02 17:33:08 -0400

    Reset HWID

--- Keys.lua
+++ Keys.lua
@@ -11,8 +11,8 @@ return {
 
         {
             Key = "28296bbb-436b-45e1-82eb-85e9f9b746d9",
-            RBLXUserId = "4124339158",
-            Hwid = "54D927F2-2E9D-4F9D-AB39-E8A02496D8B7",
+            RBLXUserId = "UserNotSet",
+            Hwid = "NotSet",
             DiscordUserId = 847907964257697803,
             Blacklisted = false,
             Activated = true
