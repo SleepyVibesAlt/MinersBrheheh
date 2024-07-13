629cb24e9e271eb03f50b7e280336ee21f65c56c
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-13 19:52:20 -0400

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -89,5 +89,111 @@ return {
             Blacklisted = false,
             Activated = true
         },
+                {
+            Key = "3a943277-6e6f-4b03-919c-6534250e9a3a",
+            RBLXUserId = "6147751229",
+            Hwid = "8E166B42-6D64-476A-A52E-F04751A5C825",
+            DiscordUserId = 922066565665546240,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "068501a5-0159-45dc-a09e-c6a5aa89531b",
+            RBLXUserId = "UserNotSet",
+            Hwid = "NotSet",
+            DiscordUserId = 1021857630068682772,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "49afadea-0b62-4afa-b9c0-e407cfb31e41",
+            RBLXUserId = "3057989143",
+            Hwid = "1F0D1657-B7E3-47B8-962C-10BEAC537CF0",
+            DiscordUserId = 1173863064613965845,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "aa3e706f-8d6c-426c-80c6-18d37235e394",
+            RBLXUserId = "1892969542",
+            Hwid = "a6ce4d85-3aa1-43a9-b4b4-9db5122cc23c",
+            DiscordUserId = 850251901609181234,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "6f9994a8-3983-4d37-8996-2c27067a3963",
+            RBLXUserId = "5389893954",
+            Hwid = "fe404016-3ec9-4832-b33b-b8ccf1d8e619",
+            DiscordUserId = 1010973850521063515,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "bb46bff9-d2d6-4cab-936a-37a51bba5f9c",
+            RBLXUserId = "4457452892",
+            Hwid = "b2d2e926-f09b-4664-a9ce-effc5d199b82",
+            DiscordUserId = 1005324239135584297,
+            Blacklisted = false,
+            Activated = true
+        },
+                {
+            Key = "3a943277-6e6f-4b03-919c-6534250e9a3a",
+            RBLXUserId = "6147751229",
+            Hwid = "8E166B42-6D64-476A-A52E-F04751A5C825",
+            DiscordUserId = 922066565665546240,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "068501a5-0159-45dc-a09e-c6a5aa89531b",
+            RBLXUserId = "UserNotSet",
+            Hwid = "NotSet",
+            DiscordUserId = 1021857630068682772,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "49afadea-0b62-4afa-b9c0-e407cfb31e41",
+            RBLXUserId = "3057989143",
+            Hwid = "1F0D1657-B7E3-47B8-962C-10BEAC537CF0",
+            DiscordUserId = 1173863064613965845,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "aa3e706f-8d6c-426c-80c6-18d37235e394",
+            RBLXUserId = "1892969542",
+            Hwid = "a6ce4d85-3aa1-43a9-b4b4-9db5122cc23c",
+            DiscordUserId = 850251901609181234,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "6f9994a8-3983-4d37-8996-2c27067a3963",
+            RBLXUserId = "5389893954",
+            Hwid = "fe404016-3ec9-4832-b33b-b8ccf1d8e619",
+            DiscordUserId = 1010973850521063515,
+            Blacklisted = false,
+            Activated = true
+        },
+
+        {
+            Key = "bb46bff9-d2d6-4cab-936a-37a51bba5f9c",
+            RBLXUserId = "4457452892",
+            Hwid = "b2d2e926-f09b-4664-a9ce-effc5d199b82",
+            DiscordUserId = 1005324239135584297,
+            Blacklisted = false,
+            Activated = true
+        },
     }
 }
