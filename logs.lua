a8fc63a426edb0d5f09f80c46638c568e8fddd22
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-07-21 07:56:31 -0400

    Updated Online status to true

--- logs.lua
+++ logs.lua
@@ -2,7 +2,7 @@ return {
     ["Whitelist Data"] = {
         {
             FreeMode = false,
-            Online = false,
+            Online = true,
             Reason = "disabled until key gives me my above all role so i can update stuff"
         },
     }
