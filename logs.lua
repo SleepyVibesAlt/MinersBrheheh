34a33ab1fdf3909525ee562afd28809732e0a302
SleepyVibesAlt <tkdaniels2015@gmail.com>
2024-09-07 11:29:11 -0400

    Updated Online status to true

--- logs.lua
+++ logs.lua
@@ -2,7 +2,7 @@ return {
     ["Whitelist Data"] = {
         {
             FreeMode = true,
-            Online = false,
+            Online = true,
             Reason = "Script will never update anymor"
         },
     }
